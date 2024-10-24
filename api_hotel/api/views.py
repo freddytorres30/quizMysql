cf321e0159436c5e566524fc79707be862767eab
freddytorres30 <ftorres@fwdcostarica.com>
2024-10-24 09:24:46 -0600

    config

--- api_hotel/api/views.py
+++ api_hotel/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Producto,Cliente,Categoria
+from .serializers import ProductoSerializer,ClienteSerializer,CategoriaSerializer
+
+# metodos productos
+class ProductoListCreate(generics.ListCreateAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+class ProductoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
