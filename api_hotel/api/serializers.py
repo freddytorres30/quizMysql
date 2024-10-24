14d4081bd84d7ec7ee527caf9ef3f8dc17a931e6
freddytorres30 <ftorres@fwdcostarica.com>
2024-10-24 14:24:11 -0600

    valid

--- api_hotel/api/serializers.py
+++ api_hotel/api/serializers.py
@@ -2,9 +2,6 @@ from rest_framework import serializers
 from .models import Clientes, Reservas, Habitaciones, Pagos
 
 
-from rest_framework import serializers
-from .models import Clientes
-
 class ClientesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clientes
