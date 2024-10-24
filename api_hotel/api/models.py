7a6dc313220d58c2c884280895798d518b35e1a6
BryanAB506 <baragon@fwdcostarica.com>
2024-10-24 14:25:24 -0600

    cambios

--- api_hotel/api/models.py
+++ api_hotel/api/models.py
@@ -1,15 +1,37 @@
+
+import re
+from django.core.exceptions import ValidationError
 from django.db import models
 
+def validar_sin_espacios(value):
+    if not value.strip():
+        raise ValidationError('El campo no puede estar vacío o contener solo espacios.')
+
 class Clientes(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=20)
-    fecha_Registro = models.DateTimeField(auto_now_add=True)
+    nombre = models.CharField(max_length=100, validators=[validar_sin_espacios])
+    apellido = models.CharField(max_length=100, validators=[validar_sin_espacios])
+    email = models.CharField(max_length=100, validators=[validar_sin_espacios])
+    telefono = models.CharField(max_length=20, validators=[validar_sin_espacios])
+    fecha_Registro = models.DateTimeField(auto_now_add=True, unique=True, validators=[validar_sin_espacios])
 
+    def clean(self):
+        # Validar que el correo no esté vacío o solo contenga espacios
+        if not self.email or self.email.isspace():
+            raise ValidationError('El correo no puede estar vacío.')
+        # Validar que el correo contenga solo caracteres válidos
+        if not re.match(r'^[\w.-]+@[\w.-]+.\w+$', self.email): 
+            raise ValidationError('El correo contiene caracteres no válidos.')
 
-    def __str__(self):
+    def save(self, args, **kwargs):
+        self.clean()
+        # Validar que el correo no exista
+        if Clientes.objects.filter(email=self.email).exists():
+            raise ValidationError('El correo ya está en uso.')
+        super().save(args, **kwargs)
+
+    def str(self):
         return self.nombre
+
 
 
 
@@ -17,8 +39,8 @@ class Reservas(models.Model):
     fecha_Entrada = models.DateField()
     cantidad_Noches = models.IntegerField()
     fecha_Reserva = models.DateField(auto_now_add=True)
-    Clientes = models.ForeignKey('Clientes', on_delete=models.CASCADE,)
-    Habitaciones = models.ForeignKey('Habitaciones', on_delete=models.CASCADE,)
+    Clientes = models.ForeignKey('Clientes', on_delete=models.CASCADE)
+    Habitaciones = models.ForeignKey('Habitaciones', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.fecha_Entrada, self.Clientes, self.Habitaciones
@@ -39,7 +61,7 @@ class Pagos(models.Model):
     monto = models.IntegerField()
     metodoPago = models.CharField(max_length=100)
     fechaPago = models.DateField()
-    Reservas = models.ForeignKey('Reservas', on_delete=models.CASCADE, related_name='Pagos')
+    Reservas = models.ForeignKey('Reservas', on_delete=models.CASCADE)
 
 
     def __str__(self):
